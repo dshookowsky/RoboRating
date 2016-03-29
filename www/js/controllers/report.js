ab80b8d5cf06526d1abc796dba11e103454b34fd
Dan Shookowsky <dshookowsky@gmail.com>
2016-03-29 10:20:35 -0400

    Remove test data

--- www/js/controllers/report.js
+++ www/js/controllers/report.js
@@ -1,22 +1,9 @@
 angular.module('roboRating.controllers')
-  .controller('ReportCtrl', function ($scope, Ratings, Teams, $ionicLoading) {
+  .controller('ReportCtrl', function ($scope, Ratings, $ionicLoading) {
       Ratings.sort().then(function (ratings) {
               $scope.ratings = ratings;
               $ionicLoading.hide();
 
-              /*
-              ratings = [];
-              teams = Teams.all();
-
-              for (var teamIndex = 0; teamIndex < teams.length; teamIndex++) {
-                  var team = teams[teamIndex];
-                  ratings.push({
-                      teamNumber: team.id,
-                      teamName: team.name,
-                      averagePoints: Math.floor(Math.random() * 500) + 1
-                  });
-              }
-              */
               $scope.graphData = ratings;
       });
   });
