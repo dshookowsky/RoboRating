939eafc42559942d740b7fb2bce72b814837176a
Dan Shookowsky <dshookowsky@gmail.com>
2016-04-22 11:38:30 -0400

    Fix build script

--- release.sh
+++ release.sh
@@ -1,3 +1,3 @@
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ~/keystore/practicaldeveloper.keystore platforms/android/build/outputs/apk/android-release-unsigned.apk practicaldeveloper.net
-del platforms/android/build/outputs/apk/RoboRating-Release.apk
+rm platforms/android/build/outputs/apk/RoboRating-Release.apk
 ~/Library/Android/sdk/build-tools/23.0.2/zipalign -v 4 platforms/android/build/outputs/apk/android-release-unsigned.apk platforms/android/build/outputs/apk/RoboRating-Release.apk
