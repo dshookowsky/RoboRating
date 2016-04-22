3b8c73c125cec66eddcead0964e263b700c45f0e
Dan Shookowsky <dshookowsky@gmail.com>
2016-04-22 11:39:46 -0400

    Allow users to add multiple teams at once

--- www/js/controllers/newRating.js
+++ www/js/controllers/newRating.js
@@ -1,39 +1,56 @@
 angular.module('roboRating.controllers')
 .controller('NewRatingCtrl', function($scope, $ionicHistory, Ratings, Teams, Locations) {
-      $scope.teams = Teams.all();
-      $scope.locations = Locations.all();
+    $scope.teams = Teams.all();
+    $scope.locations = Locations.all();
 
-      $scope.rating = {
-          ratingId: null,
-          roundNumbe: null,
-          team: {},
-          alliance: "Blue",
-          hasAutonomous: false,
-          rescueBeacon: false,
-          autonomousClimbers: null,
-          autonomousParking: null,
-          consistency: null,
-          lowDebris: null,
-          midDebris: null,
-          highDebris: null,
-          teleopParking: null,
-          scoresClimbers: null,
-          ziplineClimbers: null,
-          scoresDebris: false,
-          debrisInFloor: null,
-          endgameParking: null,
-          allClear: false,
-          totalPoints: null,
-          overallConsistency: null,
-          driverControl: null,
-          climbSpeed: null,
-          endurance: null,
-          notes: null,
-          complete: false
-      };
+    var round = {
+        number: null,
+        ratings: []
+    };
 
-      $scope.insert = function () {
-          Ratings.insert($scope.rating);
-          $ionicHistory.goBack();
-      };
-  });
+    for (var index=0; index < 4; index++) {
+        round.ratings.push({
+            ratingId: null,
+            roundNumber: null,
+            team: {},
+            alliance: "Blue",
+            hasAutonomous: false,
+            rescueBeacon: false,
+            autonomousClimbers: null,
+            autonomousParking: null,
+            consistency: null,
+            lowDebris: null,
+            midDebris: null,
+            highDebris: null,
+            teleopParking: null,
+            scoresClimbers: null,
+            ziplineClimbers: null,
+            scoresDebris: false,
+            debrisInFloor: null,
+            endgameParking: null,
+            allClear: false,
+            totalPoints: null,
+            overallConsistency: null,
+            driverControl: null,
+            climbSpeed: null,
+            endurance: null,
+            notes: null,
+            complete: false
+        });
+    }
+    $scope.round = round;
+
+    $scope.insert = function () {
+        var roundNumber = $scope.round.number;
+
+        for (var index=0; index < 4; index++) {
+            var rating = $scope.round.ratings[index];
+
+            if (rating.team.id && roundNumber != null) {
+                rating.roundNumber = roundNumber;
+                Ratings.insert(rating);
+            }
+        }
+        $ionicHistory.goBack();
+    };
+});
