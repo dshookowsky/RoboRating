f5549bbcc8bfc02f41136eec47c4550eec1e085c
Dan Shookowsky <dshookowsky@gmail.com>
2017-01-16 21:46:58 -0500

    Removing script

--- release.sh
+++
@@ -1,3 +0,0 @@
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ~/keystore/practicaldeveloper.keystore platforms/android/build/outputs/apk/android-release-unsigned.apk practicaldeveloper.net
-rm platforms/android/build/outputs/apk/RoboRating-Release.apk
-~/Android/Sdk/build-tools/25.0.2/zipalign -v 4 platforms/android/build/outputs/apk/android-release-unsigned.apk platforms/android/build/outputs/apk/RoboRating-Release.apk
